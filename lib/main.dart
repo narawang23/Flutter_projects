cfb73f177a9181d5d3cc6937152f6c742b1269ff
Nara Wang <nara.wang04@gmail.com>
2024-05-26 16:04:57 -0400

    Reconfigure files for LFS tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,83 +25,158 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   String imageSource = "images/question-mark.png";
+  bool _credentialsLoaded = false;
+
+  Future<void> _saveCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', _usernameController.text);
+    await prefs.setString('password', _passwordController.text);
+  }
+
+  Future<void> _clearCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('username');
+    await prefs.remove('password');
+    _usernameController.clear();
+    _passwordController.clear();
+    setState(() {
+      _credentialsLoaded = false;
+    });
+  }
+
+  Future<void> _showSaveDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Save Credentials?'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Would you like to save your username and password for next time?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                _saveCredentials();
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                _clearCredentials();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   void _login() {
     setState(() {
       if (_passwordController.text == "QWERTY123") {
-        imageSource = "images/idea.png"; // Change to light bulb image
+        imageSource = "images/idea.png";
       } else {
-        imageSource = "images/stop.png"; // Change to stop sign image
+        imageSource = "images/stop.png";
       }
     });
+    _showSaveDialog();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCredentials();
+  }
+
+  Future<void> _loadCredentials() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? username = prefs.getString('username');
+    String? password = prefs.getString('password');
+
+    if (username != null && password != null) {
+      _usernameController.text = username;
+      _passwordController.text = password;
+      _credentialsLoaded = true;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Previous credentials loaded.'),
+          action: SnackBarAction(
+            label: 'Clear Saved Data',
+            onPressed: _clearCredentials,
+          ),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  hintText: "Username",
-                  border: OutlineInputBorder(),
-                  labelText: "Login",
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                border: OutlineInputBorder(),
+                labelText: "Login",
               ),
-              SizedBox(height: 20), // Add some space between the fields
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                border: OutlineInputBorder(),
+                labelText: "Password",
               ),
-              SizedBox(height: 20), // Add some space between the fields and button
-          ElevatedButton(
-            onPressed: _login,
-            child: Container(
-              // color: Colors.yellow, // Background color for the text
-              padding: EdgeInsets.all(8.0),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Container(
+                padding: EdgeInsets.all(8.0),
                 child: Text(
                   'Login',
                   style: TextStyle(
-                    fontSize: 24.0, // Adjusts the font size
-                    color: Colors.blue, // Adjusts the font color
+                    fontSize: 24.0,
+                    color: Colors.blue,
                   ),
                 ),
               ),
-          ),
-              Image.asset(
-                imageSource,
-                width: 300,
-                height: 300,
-              ),
-            ],
-          ),
+            ),
+            Image.asset(
+              imageSource,
+              width: 300,
+              height: 300,
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
