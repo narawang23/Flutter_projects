eb23e2f011c5a3d7c2b31d6f1411607cc641ab7a
Nara Wang <nara.wang04@gmail.com>
2024-07-02 18:18:40 -0400

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
-
-import 'secondPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,32 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // list all pages
-      routes:{
-        //keys:     //values
-        '/pageOne'  :  (context) => MyHomePage(title: "Flutter Demo Home Page"),
-        '/pageTwo'  :  (context) {
-          return SecondPage();
-        }
-      },
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/pageOne',
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class DataRepository{
-  static String loginName="";
-  static String password="";
 }
 
 class MyHomePage extends StatefulWidget {
@@ -48,173 +30,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _counter = 0.0;
-  var myFontSize = 25.0;
-  // var isChecked = false;
 
-  late TextEditingController _loginController;
-  late TextEditingController _passwordController;
-  late EncryptedSharedPreferences savedData;
+  List<String> words = [
+    'Item 1',
 
-  @override
-  void initState() {
-    super.initState();
-    _loginController = TextEditingController();
-    _passwordController = TextEditingController();
-    savedData = EncryptedSharedPreferences();
-    _loadSavedData();
+  ];
 
-  }
-
-  @override
-  void dispose() {
-    _loginController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  void _loadSavedData() async {
-    String? savedLogin = await savedData.getString("login");
-    String? savedPassword = await savedData.getString("password");
-
-    if (savedLogin != null && savedLogin.isNotEmpty && savedPassword != null && savedPassword.isNotEmpty) {
-
-      setState(() {
-        _loginController.text = savedLogin;
-        _passwordController.text = savedPassword;
-      });
-    }
-  }
-
-
-  void _validateAndLogin()  async{
-    String? savedLogin = await savedData.getString("login");
-    String? savedPassword = await savedData.getString("password");
-    if (_loginController.text == savedLogin &&
-        _passwordController.text == savedPassword ) {
-      Navigator.pushNamed(context, '/pageTwo').then((_) {
-        final snackBar = SnackBar(
-          content: Text('Welcome Back, ${_loginController.text}'),
+  final TextEditingController _inputController = TextEditingController();
+//show the ask when delete
+  void _showDeleteDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Item'),
+          content: const Text('Are you sure you want to delete this item?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  words.removeAt(index);
+                });
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Yes'),
+            ),
+          ],
         );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      });
-    }
-    else {
-      showDialog<String>(
-        context: context,
-        builder: (BuildContext context) =>
-            AlertDialog(
-              title: const Text(
-                  'Would you like to save the username and password?'),
-              actions: <Widget>[
-                FilledButton(
-                  onPressed: () {
-                    var userTyped = _loginController.value.text;
-                    var passwordTyped = _passwordController.value.text;
-                    savedData.setString("login", userTyped);
-                    savedData.setString("password", passwordTyped);
-                    Navigator.pop(context);
-                  },
-                  child: const Text("OK"),
-                ),
-                FilledButton(
-                  onPressed: () {
-                    savedData.setString("login", "");
-                    savedData.setString("password", "");
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Cancel"),
-                ),
-              ],
-            ),
-      );
-    }
-  }
-
-
-  void buttonClicked() {
-    if (_loginController.text.isNotEmpty && _passwordController.text.isNotEmpty)
-    { _validateAndLogin();
-      // Navigator.pushNamed(context, '/pageTwo');
-    } else {
-      showDialog<String>(
-        context: context,
-        builder: (BuildContext context) =>
-            AlertDialog(
-              title: const Text(
-                  'Would you like to save the username and password?'),
-              actions: <Widget>[
-                FilledButton(
-                  onPressed: () {
-                    var userTyped = _loginController.value.text;
-                    var passwordTyped = _passwordController.value.text;
-                    savedData.setString("login", userTyped);
-                    savedData.setString("password", passwordTyped);
-                    Navigator.pop(context);
-                  },
-                  child: const Text("OK"),
-                ),
-                FilledButton(
-                  onPressed: () {
-                    savedData.setString("login", "");
-                    savedData.setString("password", "");
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Cancel"),
-                ),
-              ],
-            ),
-      );
-    }
+      },
+    );
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '',
-              style: TextStyle(fontSize: myFontSize, color: Colors.deepPurple),
-            ),
-            Flexible(
-              child: TextField(
-                controller: _loginController,
-                decoration: const InputDecoration(
-                  hintText: "Login",
-                  border: OutlineInputBorder(),
-                  labelText: "Login",
-                ),
-              ),
-            ),
-            Flexible(
-              child: TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "Password",
-                  border: OutlineInputBorder(),
-                  labelText: "Password",
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-            ElevatedButton(
-              onPressed: buttonClicked,
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: myFontSize),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
 
-  }
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(child: TextField(controller: _inputController,)),
+                  ElevatedButton(onPressed: (){
+                    if(_inputController.value.text.isNotEmpty) {
+                      setState(() {
+                        words.add(_inputController.value.text);
+                        _inputController.text = "";
+                      });
+                    }
+                  }, child: Text('Add Item'))
+                ],
+              ),
+              Expanded(
+                child: words.isEmpty
+                    ? Center(child: Text('There are no items in the list'))
+                    :ListView.builder(itemCount:words.length,
+                    itemBuilder: (context,index){return GestureDetector(
+                      onTap:() {
+                        setState(() {
+                          words.removeAt(index);
+                        });
+                      },
+                      //long press
+                      onLongPress: () {
+                        _showDeleteDialog(index);
+                      },
+                      child: Container(
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Row number: $index'),
+                            Text(words[index])
+                          ],
+                        ),
+                      ),
+                    );}),
+              )
+            ],
+          ),
+        ),
+
+       );
+    }
 }
